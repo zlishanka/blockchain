38796dd89fe765d7deede26e3f40597bcb8bcfce
Roy Zheng <royzheng@Roy-Zhengs-MacBook-Pro.local>
2024-01-19 13:16:09 -0800

    Update FundMe contract to add price feed oracle service inside constructor

--- web3-demos/brownie_fund_me/scripts/deploy.py
+++ web3-demos/brownie_fund_me/scripts/deploy.py
@@ -5,8 +5,8 @@ import os
 
 def depoly_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
-    # fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    fund_me = FundMe.deploy("0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e", {"from": account})
+    # fund_me = FundMe.deploy({"from": account}, publish_source=True,)
     print(f"contract deployed to {fund_me.address}")
   
 def main():
